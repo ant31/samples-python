f3a8f6b63cb101ead79483d7288bf7b1069922a2
ant31 <2t.antoine@gmail.com>
2023-03-29 19:20:57 +0200

    Temporal looper: activity_sticky_queues/worker.py

--- activity_sticky_queues/worker.py
+++ activity_sticky_queues/worker.py
@@ -1,19 +1,20 @@
-import asyncio
-import logging  # noqa
 import random
 from typing import List
 from uuid import UUID
 
 from temporalio import activity
-from temporalio.client import Client
-from temporalio.worker import Worker
 
 from activity_sticky_queues import tasks
+from temporalloop.config import Config, WorkerConfig
+from temporalloop.main import run
 
-interrupt_event = asyncio.Event()
 
+@activity.defn(name="get_available_task_queue")
+async def select_task_queue_random() -> str:
+    """Randomly assign the job to a queue"""
+    return random.choice(task_queues)
 
-async def main():
+def main():
     # Uncomment the line below to see logging
     # logging.basicConfig(level=logging.INFO)
 
@@ -26,49 +27,30 @@ async def main():
         for _ in range(5)
     ]
 
-    @activity.defn(name="get_available_task_queue")
-    async def select_task_queue_random() -> str:
-        """Randomly assign the job to a queue"""
-        return random.choice(task_queues)
-
-    # Start client
-    client = await Client.connect("localhost:7233")
-
-    # Run a worker to distribute the workflows
-    run_futures = []
-    handle = Worker(
-        client,
-        task_queue="activity_sticky_queue-distribution-queue",
+    # worker to distribute the workflows
+    distributor_worker = WorkerConfig(name="base-queue-distributor",
+        queue="activity_sticky_queue-distribution-queue",
         workflows=[tasks.FileProcessing],
-        activities=[select_task_queue_random],
-    )
-    run_futures.append(handle.run())
-    print("Base worker started")
+        activities=[select_task_queue_random])
 
-    # Run the workers for the individual task queues
+    # workers to process queues
+    processing_workers = []
     for queue_id in task_queues:
-        handle = Worker(
-            client,
-            task_queue=queue_id,
-            activities=[
-                tasks.download_file_to_worker_filesystem,
-                tasks.work_on_file_in_worker_filesystem,
-                tasks.clean_up_file_from_worker_filesystem,
-            ],
-        )
-        run_futures.append(handle.run())
-        # Wait until interrupted
-        print(f"Worker {queue_id} started")
+        processing_workers.append(
+            WorkerConfig(name="base-queue-distributor",
+                         queue=queue_id,
+                         activities=[
+                             tasks.download_file_to_worker_filesystem,
+                             tasks.work_on_file_in_worker_filesystem,
+                             tasks.clean_up_file_from_worker_filesystem,
+                         ]))
 
-    print("All workers started, ctrl+c to exit")
-    await asyncio.gather(*run_futures)
+    run(Config(
+        host="localhost:7233",
+        workers=[distributor_worker] + processing_workers))
+
+
 
 
 if __name__ == "__main__":
-    loop = asyncio.new_event_loop()
-    try:
-        loop.run_until_complete(main())
-    except KeyboardInterrupt:
-        interrupt_event.set()
-        loop.run_until_complete(loop.shutdown_asyncgens())
-        print("\nShutting down workers")
+    main()
